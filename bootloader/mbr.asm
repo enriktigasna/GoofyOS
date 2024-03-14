3904f1f9eab34161e5085c32d92ce94917ec8d54
enriktigasna <maxim.pavlenko2011@gmail.com>
2024-03-14 18:46:55 +0100

    Added FAT32 BPB Headers

--- bootloader/mbr.asm
+++ bootloader/mbr.asm
@@ -1,6 +1,42 @@
 bits 16
 org 0x7c00
 
+;;; JUMP TO BOOT SECTOR
+jmp short start
+nop ; pad out to 3 bytes
+
+;;; BPB
+OEMLabel db "GOOFY-OS"
+BytesPerSector dw 512
+SectorsPerCluster db 1
+ReservedSectors dw 0x20
+NumberOfFATs db 2
+RootEntries dw 0
+NumberOfSectors dw 0
+MediaDescriptor db 0xf8
+SectorsPerFAT dw 0
+SectorsPerTrack dw 0x3d
+SectorsPerHead dw 2
+HiddenSectors dd 0
+TotalSectors dd 0x2000 ;; 4 MB
+BigSectorsPerFAT dd 64
+Flags dw 0
+FSVersion dw 0
+RootDirectoryStart dd 160
+FSInfoSector dw 1
+BackupBootSector dw 6
+
+TIMES 12 db 0 ;; Reserved
+
+DriveNumber db 0x00
+ReservedByte db 0
+Signature db 0x29
+VolumeID dd 0x12345678
+VolumeLabel db "GOOFYOS    "
+FileSystem db "FAT32   "
+
+;;; BOOT SECTOR
+start:
 KERNEL_OFFSET equ 0x1000
 mov [BOOT_DRIVE], dl
 
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,7 @@
+// TODO:
+// Load FAT32 file system
+// Load tty driver from file system
+// Start shell
 void _start(){
     char* video_memory = (char*) 0xb8000;
     *video_memory = 'E';
